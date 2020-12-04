cd911c63c3aef4b2f2380a20c7b11d5d1bb0f8e3
simottardi <simottardi@gmail.com>
2020-12-04 19:36:00 +0100

    first material UI adaptation for the patient dashboard

--- src/components/PatientCard/index.js
+++ src/components/PatientCard/index.js
@@ -2,13 +2,131 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../store/user/selectors";
 
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  title: {
+    fontSize: 14,
+  },
+}));
+
 export default function PatientCard() {
+  const classes = useStyles();
   const { token, email, id, address, createdAt, name } = useSelector(
     selectUser
   );
+  console.log("createdAt", createdAt);
 
+  //Splitting the date and time string into date and time
+
+  let myDateSplit = (createdAtParam) => {
+    if (createdAtParam !== undefined) {
+      createdAtParam.split("T");
+    } else {
+      return "loading data";
+    }
+  };
+
+  myDateSplit(createdAt);
+  const myDate = myDateSplit[0];
+  console.log("mydate", myDateSplit, myDateSplit[0]);
   return (
-    <div className="container pt-12 mb-4 ">
+    <div>
+      {/*      <div className={classes.root}> */}
+      <Accordion
+        style={{
+          paddingTop: 12,
+          paddingBottom: 12,
+          paddingRight: 12,
+          paddingLeft: 12,
+          backgroundColor: "#decbf5",
+          marginTop: 6,
+          marginBottom: 12,
+        }}
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography
+            variant="h6"
+            /*             justify="left"
+            align="left" */
+            /*            className={classes.heading} */
+            /*             style={{ marginBottom: 12, MarginTop: 6 }} */
+          >
+            Your details
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails /* justify="center" alignItem="center" */>
+          <Typography /* justify="center" align="center" */>
+            patient name: {name}
+            <br />
+            patient ID: {id} <br />
+            email: {email} <br />
+            address: {address} <br />
+            Patient since:{createdAt}
+          </Typography>{" "}
+        </AccordionDetails>
+      </Accordion>
+      {/*   </div> */}
+
+      {/*       <Card
+        style={{
+          paddingTop: 12,
+          paddingBottom: 12,
+          paddingRight: 12,
+          paddingLeft: 12,
+          backgroundColor: "#decbf5",
+          marginTop: 6,
+          marginBottom: 12,
+        }}
+      >
+        {" "}
+        <Typography
+          variant="h6"
+          justify="left"
+          align="left"
+          style={{ marginBottom: 12, MarginTop: 6 }}
+        >
+          Your details
+        </Typography>
+        <CardContent
+          style={{
+            paddingBottom: 3,
+          }}
+        >
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            patient name: {name}
+            <br />
+            patient ID: {id} <br />
+            email: {email} <br />
+            address: {address} <br />
+            Patient since:{createdAt}
+          </Typography>
+        </CardContent>
+      </Card> */}
+    </div>
+    /*     <div className="container pt-12 mb-4 ">
       <div className="card-deck text-center">
         <div className="card shadow-sm">
           <div className="card-header">
@@ -25,6 +143,6 @@ export default function PatientCard() {
           </div>
         </div>
       </div>
-    </div>
+    </div> */
   );
 }
--- src/pages/MyHomepage/index-bootstrap.js
+++ src/pages/MyHomepage/index-bootstrap.js
@@ -5,23 +5,11 @@ import { selectToday } from "../../store/appState/selectors";
 import { useHistory, Link } from "react-router-dom";
 import { fetchPatientHistory } from "../../store/patientHistory/actions";
 
-/* import Container from "react-bootstrap/Container"; */
+import Container from "react-bootstrap/Container";
 import { Col } from "react-bootstrap";
 import PatientCard from "../../components/PatientCard";
 import Chart from "../../components/Chart";
 import { selectPatientHistory } from "../../store/patientHistory/selectors";
-
-import { makeStyles } from "@material-ui/core/styles";
-import {
-  Container,
-  Card,
-  Grid,
-  CardActions,
-  CardContent,
-  Button,
-  Typography,
-  colors,
-} from "@material-ui/core";
 
 export default function MyHomepage() {
   const { token, name } = useSelector(selectUser);
@@ -46,66 +34,23 @@ export default function MyHomepage() {
   const data = patientHistoryData.reverse();
 
   return (
-    <Container align="center" maxWidth="sm" fixed style={{ marginTop: 12 }}>
-      {/*     //  <Container as={Col} md={{ span: 6, offset: 3 }} className="mt-2 mb-2 "> */}
-      <Container>
-        <Typography
-          variant="h5"
-          justify="center"
-          align="center"
-          style={{ marginBottom: 12 }}
-        >
-          Patient homepage
-        </Typography>
-        {/*         <h1 className="text-white">Patient homepage </h1> */}
-        {/* <h5 className="text-white">
+    <Container>
+      <Container as={Col} md={{ span: 6, offset: 3 }} className="mt-2 mb-2 ">
+        <h1 className="text-white">Patient homepage </h1>
+        <h5 className="text-white">
           Welcome back <strong> {name}</strong>
         </h5>
         <p className="text-white">
           Today is <strong>{today}</strong>
-        </p> */}
-        <Typography
-          variant="body"
-          justify="center"
-          align="center"
-          style={{ marginBottom: 12 }}
-        >
-          Welcome back <strong> {name}</strong>
-        </Typography>
-        <br></br>
-        <Typography
-          variant="body"
-          justify="center"
-          align="center"
-          style={{ marginBottom: 12 }}
-        >
-          Today is <strong>{today}</strong>
-        </Typography>
-        {/*     <Link to="/newday" className="text-white mb-2">
+        </p>
+        <Link to="/newday" className="text-white mb-2">
           <button
             type="button"
             className="mb-2 btn btn-lg btn-block btn-outline-light"
           >
             Click here to create a new day in your journal.
           </button>
-        </Link> */}
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          style={{ marginTop: 12, marginBottom: 12 }}
-        >
-          <Link
-            to="/newday"
-            className="text-white mb-2"
-            style={{ textDecoration: "none" }}
-          >
-            <Button size="small" variant="contained" color="primary">
-              Create a new day in your journal
-            </Button>
-          </Link>
-        </Grid>
+        </Link>
         <PatientCard />
       </Container>
       <Container>
